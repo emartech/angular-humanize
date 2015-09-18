992e2a2cc96cb57968bcacbab88196b6c1213208
Viktor Somodi <somodi.viktor@gmail.com>
2015-09-18 05:56:53 +0200

    fix factory

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const humanize = require('humanize-num');
 module.exports = function(angular) {
   angular
     .module(MODULE_NAME, [])
-    .filter('humanize', humanize);
+    .filter('humanize', () => humanize);
   return MODULE_NAME;
 };
