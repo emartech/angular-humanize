e1ac06ed630b3e7eef2142e0a7be7fe001156014
Somodi Viktor <somodi.viktor@gmail.com>
2015-10-16 14:15:14 +0200

    fix(module): humanize filter declaration

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var humanize = require('humanize-num');
 module.exports = function(angular) {
   angular
     .module(MODULE_NAME, [])
-    .filter('humanize', [humanize]);
+    .filter('humanize', [function() {
+      return humanize;
+    }]);
   return MODULE_NAME;
 };
